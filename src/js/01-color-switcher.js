f61286df4b3adea7228d03652e232a370ce12e99
DanilStan <stanislavov.danik@gmail.com>
2022-09-13 15:34:56 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
+buttonStop.disabled = true;
 
+buttonStart.addEventListener('click', onButtonStart);
+buttonStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onButtonStop() {
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+  document.body.style.backgroundColor = '#fff';
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
